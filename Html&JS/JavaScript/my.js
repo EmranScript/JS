bfe9d14c30be079c5d834f43d41bbf8ab796feac
EmranScript <emranstudentt@gmail.com>
2025-03-26 16:53:39 +0200

    Add files via upload

---
+++ Html&JS/JavaScript/my.js
@@ -0,0 +1,23 @@
+'use strict';
+// document.writeln("this is me testing JS");
+
+
+// document.writeln("well done its easy shouldnot be peoud ");
+
+// console.log("this is written in console");
+// const myinfo= { firstname : " emran " , lastname : " kewan "}
+// console.log(myinfo);
+// const array = ["potato " , "banana " , "apple"];
+// for(let i = 0 ; i<=array.length ; i++)
+// {
+//  console.log(array[i]);
+
+// }
+// let name = prompt('whats your name');
+// console.log(name);
+// let custmor = confirm("do you want to delete this itme ? ");
+if (confirm("do you want to delete this itme ? "))
+console.log('item deleted ');
+else 
+
+console.log('item have not been deleted ');
